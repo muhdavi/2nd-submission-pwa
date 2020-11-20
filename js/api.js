dc64eafbbcfca02a0b3744748dcdc991b037ea24
Muhammad Davi <davi_muhammad@yahoo.com>
2020-11-20 20:15:21 +0700

    third submit

--- js/api.js
+++ js/api.js
@@ -1,32 +1,69 @@
+const id_liga = 2021;
+const base_url = 'https://api.football-data.org/v2';
+const endpoint_standings = `${base_url}/competitions/${id_liga}/standings`;
+const endpoint_team = `${base_url}/teams/`;
+
+function status(response) {
+    if (response.status !== 200) {
+        console.log("[API.js][status] Error : " + response.status);
+        return Promise.reject(new Error(response.statusText));
+    } else {
+        return Promise.resolve(response);
+    }
+}
+
+function json(response) {
+    return response.json();
+}
+
 const Api = {
     getStandings() {
-        if ("caches" in window) {
-            caches.match('https://api.football-data.org/v2/').then(function (response) {
-                if (response) {
-                    response.json().then(function (data) {
-                        console.log("Competition Data: " + data);
-                        return data;
-                    })
+        return new Promise(function(resolve, reject) {
+            if ("caches" in window) {
+                caches.match(endpoint_standings).then(function(response) {
+                    if (response) {
+                        response.json().then(function(data) {
+                            resolve(data);
+                        });
+                    }
+                });
+            }
+
+            fetch(endpoint_standings, {
+                headers: {
+                    'X-Auth-Token': '9ba167608e214031af211ba574b14b90'
                 }
             })
-        }
-
-        return fetch('https://api.football-data.org/v2/competitions/2021/standings', {
-            headers: {
-                'X-Auth-Token': '9ba167608e214031af211ba574b14b90'
-            }
-        })
-            .then(response => response.json())
-            .catch(err => console.log(err))
+                .then(status)
+                .then(json)
+                .then(function(data) {
+                    resolve(data);
+                });
+        });
     },
 
     getTeam(id) {
-        return fetch(`https://api.football-data.org/v2/teams/${id}`, {
-            headers: {
-                'X-Auth-Token': '9ba167608e214031af211ba574b14b90'
+        return new Promise(function (resolve, reject) {
+            if ("caches" in window) {
+                caches.match(endpoint_team + id).then(function (response) {
+                    if (response) {
+                        response.json().then(function (data) {
+                            resolve(data);
+                        });
+                    }
+                });
             }
-        })
-            .then(response => response.json())
-            .catch(err => console.log(err))
+
+            fetch(endpoint_team + id, {
+                headers: {
+                    'X-Auth-Token': '9ba167608e214031af211ba574b14b90'
+                }
+            })
+                .then(status)
+                .then(json)
+                .then(function (data) {
+                    resolve(data);
+                })
+        });
     }
 }
